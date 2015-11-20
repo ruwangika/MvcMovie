7acfdc622bf94b16572f47b6ca2340c0e65c7a0e
ruwangika <ruwangikagunawardana@gmail.com>
2015-11-20 11:28:29 +0530

    First commit

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloController : Controller
+    {
+        //
+        // GET: /Hello/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+//        public string Index() {
+//            return "This is my <b>default</b> action...";
+//        }
+        public ActionResult Welcome(string name, int numTimes = 1) {
+//            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+
+    }
+}
